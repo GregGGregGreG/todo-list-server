732e1f55b6133ddb6077cc74ac829fc8d6379cf9
greg <truegreg13@mail.ru>
2015-08-10 15:41:03 +0300

    Add fin by name starting with method in user repository

--- src/main/java/edu/greg/todolist/todo/persistence/repository/UserRepository.java
+++ src/main/java/edu/greg/todolist/todo/persistence/repository/UserRepository.java
@@ -3,6 +3,8 @@ package edu.greg.todolist.todo.persistence.repository;
 import edu.greg.todolist.todo.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by greg on 28.06.15.
  */
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User,Integer> {
     User findByName(String name);
 
     User findByEmail(String email);
+
+    List<User> finByNameStartingWith(String name);
 }
