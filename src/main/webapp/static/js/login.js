bff645d96058f6d1b550924b5a1626c52e4615b5
greg <truegreg13@mail.ru>
2015-08-31 18:42:40 +0300

    Create login.css

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -1,37 +1,15 @@
-//Set footer style
-$(function () {
-    $('html').css({
-        'position': 'relative',
-        'min-height': '100%'
-    });
-    /* Margin bottom by footer height */
-    $('body').css({
-        'margin-bottom': '60px'
-    });
-    $('footer').css({
-        'position': 'absolute',
-        'bottom': '10px',
-        'width': '100%'
-    });
-});
-// show area login
-$(".account-wall").slideToggle("slow");
-
 // show reset button imput
-$('span.input-text input')
+$('.form-group input')
     .focus(function () {
-        $(this).closest('span').find('.reset-input').slideDown();
+        $(this).closest('.form-group').find('.reset-input').slideDown();
     }).blur(function () {
-        var $span = $(this).closest('span');
-        var $input = $span.find('input');
-
+        var $span = $(this).closest('.form-group');
         $span.find('.reset-input').slideUp();
     });
-
 //reset-input action
 $('.reset-input').on('click', function () {
     //find input
-    var $input = $(this).closest('span').find('input');
+    var $input = $(this).closest('.form-group').find('input');
 
     if ($input.val() == '') {
         $input.blur();
@@ -39,4 +17,4 @@ $('.reset-input').on('click', function () {
         $input.val('');
         $input.focus();
     }
-})
+});
